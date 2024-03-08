faac4cc0cdb40e00fe0c72a1ce082cae03994e06
roushan-sheik <roushansheik@gmail.com>
2024-03-08 07:23:56 +0600

    footer: subscribe button added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,25 @@
 import React from "react";
 import { FaRegHeart } from "react-icons/fa6";
+import { GoMail } from "react-icons/go";
 
 const Footer = () => {
   return (
     <footer>
       {/* footer parent div  */}
-      <div className="flex lg:flex-row flex-col ">
+      <div className="flex lg:flex-row flex-col py-8 ">
         {/* left box  */}
-        <div>
-          <div>
-            <FaRegHeart />
-            <p>Support</p>
+        <div className="flex  gap-4 items-center">
+          <div className="">
+            <a href="#" className="flex  gap-2 items-center cursor-pointer">
+              <GoMail />
+              <p className="text-base black-secondary">Subscribe</p>
+            </a>
           </div>
           <div>
-            <FaRegHeart />
-            <p>Support</p>
+            <a href="#" className="flex  gap-2 items-center cursor-pointer">
+              <FaRegHeart />
+              <p className="text-base black-secondary">Support</p>
+            </a>
           </div>
         </div>
         {/* center box  */}
