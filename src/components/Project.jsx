8f572956f88980a67585341b5938f441cf758dbf
roushan-sheik <roushansheik@gmail.com>
2024-03-08 11:56:57 +0600

    displayed projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -13,8 +13,21 @@ const Project = ({ project }) => {
     gitHubRepo,
     achievements,
   } = project;
-   
-  return <div></div>;
+
+  return (
+    <div className="bg-white p-4 rounded-lg border border-gray-200 shadow-md">
+      {/* image-box  */}
+      <div className="h-[250px]  ">
+        <a href={liveURL?.url}>
+          <img className="h-full object-cover" src={cover.srcPath} alt="Img" />
+        </a>
+      </div>
+      {/* content-box  */}
+      <div>
+        <h2>{name}</h2>
+      </div>
+    </div>
+  );
 };
 
 export default Project;
