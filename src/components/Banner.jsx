aea003b1b02248b0b352927d3a319ecfab38cd51
roushan-sheik <roushansheik@gmail.com>
2024-03-08 08:13:34 +0600

    Home section responsive design added

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -27,26 +27,32 @@ const Banner = () => {
         {/* content box parent */}
         <div>
           {/* title box */}
-          <div className="flex text-3xl font-bold gap-2">
-            <h2 className="capitalize primary_black">{firstName} </h2>
-            <h2 className="capitalize text-[#8a817c]">{lastName}</h2>
+          <div className="flex text-3xl justify-center lg:justify-start font-bold  gap-2">
+            <h2 className="capitalize primary_black  ">{firstName} </h2>
+            <h2 className="capitalize text-[#8a817c] ">{lastName}</h2>
           </div>
-          <h4 className="secondary_black py-2 ">{position}</h4>
-          <h4 className="secondary_black py-2">{summary}</h4>
-          <h4 className="secondary_black py-2">{location?.name}</h4>
+          <h4 className="secondary_black flex justify-center lg:justify-start  py-2 ">
+            {position}
+          </h4>
+          <h4 className="secondary_black flex justify-center lg:justify-start py-2">
+            {summary}
+          </h4>
+          <h4 className="secondary_black flex justify-center lg:justify-start py-2">
+            {location?.name}
+          </h4>
           {/* button box container  */}
-          <div className="flex py-2 gap-4">
+          <div className="flex justify-center lg:justify-start py-3 gap-4">
             {tags.map((tag) => {
               return (
-                <button className="p-1 text-[.875rem] bg-gray-200 rounded">
+                <button className="py-1 px-2 text-[.875rem] bg-gray-200 rounded">
                   {tag.name}
                 </button>
               );
             })}
           </div>
           {/* Social links  */}
-          <div className="flex py-2 gap-2">
-            <div className="flex gap-2">
+          <div className="flex  justify-center lg:justify-start py-3 gap-2">
+            <div className="flex lg:gap-4 gap-2">
               {socialLinks.map((social) => {
                 return (
                   <a
@@ -64,7 +70,7 @@ const Banner = () => {
             </button>
           </div>
           {/* my resume  */}
-          <div className="my-2">
+          <div className="my-2 flex justify-center lg:justify-start">
             <a
               href="https://drive.google.com/file/d/1garOnqLJR8VGG7fxKw42pM7l1RWg7E8P/view?usp=sharing"
               download
@@ -72,7 +78,7 @@ const Banner = () => {
             >
               <button
                 type="button"
-                className="group relative h-12 w-32 overflow-hidden border-2 border-gray-300 rounded-lg text-base primary_black hover:font-[500]"
+                className="group relative lg:h-12 h-10 lg:w-32 w-24 overflow-hidden border-2 border-gray-300 rounded-lg text-base primary_black hover:font-[500]"
               >
                 <span className="bg-gray-300  ease-in absolute w-[57%]  -translate-x-full group-hover:translate-x-0 -left-2 top-0 bottom-0 duration-300 -z-10 skew-x-12"></span>
                 <span className="bg-gray-200 ease-in absolute w-[55%]  translate-x-full group-hover:translate-x-0 -right-2 top-0 bottom-0 duration-300 skew-x-12 -z-10"></span>
