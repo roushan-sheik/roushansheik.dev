25c96ec50df0e80056354d76dd83ed8c843a4e1b
roushan-sheik <roushansheik@gmail.com>
2024-03-08 12:29:52 +0600

    projects updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer>
       {/* footer parent div  */}
-      <div className="flex lg:flex-row gap-8 mb-4 lg:gap-0 justify-between items-center flex-col py-8 ">
+      <div className="flex lg:flex-row gap-8 mt-20 mb-10 lg:gap-0 justify-between items-center flex-col  ">
         {/* left box  */}
         <div className="flex lg:flex-1 gap-4 items-center">
           <div className="">
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -15,12 +15,20 @@ const Project = ({ project }) => {
   } = project;
 
   return (
-    <div className="bg-white p-4 rounded-lg border border-gray-200 shadow-md">
+    <div className="bg-white p-4 rounded-lg border overflow-hidden border-gray-200 shadow-md">
       {/* image-box  */}
-      <div className="h-[250px]  ">
-        <a href={liveURL?.url}>
-          <img className="h-full object-cover" src={cover.srcPath} alt="Img" />
+      <div id="img_shadow_container" className="h-[250px] relative rounded-lg">
+        <a className="z-10" href={liveURL?.url}>
+          <img
+            className="h-full object-cover rounded-lg"
+            src={cover.srcPath}
+            alt="Img"
+          />
         </a>
+        {/* Hover effect div  */}
+        <div className="img_shadow absolute w-full   h-[250px]  flex  top-0  left-0 rounded-lg  justify-center items-center bg-stone-200 ">
+          <h2 className="text-base text-semibold">Visit</h2>
+        </div>
       </div>
       {/* content-box  */}
       <div>
